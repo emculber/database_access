5263a640c52fc9b3ba532708fd33187ec0cfd259
Erik <emculber@oakland.edu>
2016-06-20 15:54:23 -0400

    Added a config generator

---
+++ postgresql/postgresql_generator.go
@@ -0,0 +1,40 @@
+package postgresql_access
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GenerateConfigFile() {
+	db := DatabaseInfo{}
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Please Enter Host: ")
+	db.Host, _ = reader.ReadString('\n')
+	db.Host = strings.Replace(db.Host, "\n", "", -1)
+
+	fmt.Print("Please Enter Port: ")
+	port, _ := reader.ReadString('\n')
+	port = strings.Replace(port, "\n", "", -1)
+	db.Port, _ = strconv.Atoi(port)
+
+	fmt.Print("Please Enter Username: ")
+	db.Username, _ = reader.ReadString('\n')
+	db.Username = strings.Replace(db.Username, "\n", "", -1)
+
+	fmt.Print("Please Enter Password: ")
+	db.Password, _ = reader.ReadString('\n')
+	db.Password = strings.Replace(db.Password, "\n", "", -1)
+
+	fmt.Print("Please Enter Database Name: ")
+	db.Dbname, _ = reader.ReadString('\n')
+	db.Dbname = strings.Replace(db.Dbname, "\n", "", -1)
+
+	config := Configuration{db}
+	json_config, _ := json.Marshal(config)
+	fmt.Println(string(json_config))
+}
